988c8d30ac7dbb3a804759166606c8d4decc20d7
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-09-22 17:26:19 -0500

    Add CRT monitor FX and boot screen
    This adds a new CRT/VHS shader overlay for the computer screen and wires it into the HUD scene. The computer boot logic now advances to a home screen after startup, with adjusted loading timing and visibility toggling when the monitor is opened or closed.

--- mainWorld/computer_obj.gd
+++ mainWorld/computer_obj.gd
@@ -2,35 +2,42 @@ extends Node3D
 @onready var puterimatir: AnimationPlayer = $computerHUD/puterimatir
 @onready var player: CharacterBody3D = $"../../player"
 @onready var progress_bar: ColorRect = $computerHUD/bootScreen/progressBar
+@onready var home_screen: Sprite2D = $computerHUD/bootScreen/homeScreen
 
 var compOn:=false
 var maxBarSize:=262.025
 var buffering:=false
 var fastLoad:=false
+var onMainScreen:=false
 
 func _process(delta: float) -> void:
-	if compOn:
+	if compOn && !onMainScreen:
 		if progress_bar.size.x<maxBarSize:
-			await get_tree().create_timer(randf_range(0.5,1.0)).timeout
+			await get_tree().create_timer(randf_range(0.25,0.5)).timeout
 			if progress_bar.size.x>maxBarSize*0.25 && maxBarSize*0.8>progress_bar.size.x:
 				if randi_range(0,100)==0:
 					if !buffering:
 						buffering=true
-						await get_tree().create_timer(randf_range(3.0,6.0)).timeout
+						await get_tree().create_timer(randf_range(3.0-Manager.printerSpeedUpgrades,5.0-Manager.printerSpeedUpgrades)).timeout
 						buffering=false
 						if progress_bar.size.x<maxBarSize*0.8:
 							progress_bar.size.x+=randf_range(15,Manager.printerSpeedUpgrades*20)
 			if !buffering:
 				if progress_bar.size.x>maxBarSize*0.95 && !fastLoad:
-					progress_bar.size.x+=randf_range(0.01,Manager.printerSpeedUpgrades*0.1)
+					progress_bar.size.x+=randf_range(0.02*Manager.printerSpeedUpgrades,Manager.printerSpeedUpgrades*0.1)
 				elif progress_bar.size.x>maxBarSize*0.9 && !fastLoad:
-					progress_bar.size.x+=randf_range(0.01,Manager.printerSpeedUpgrades*0.2)
+					progress_bar.size.x+=randf_range(0.05*Manager.printerSpeedUpgrades,Manager.printerSpeedUpgrades*0.2)
 				else:
-					progress_bar.size.x+=randf_range(0.1,Manager.printerSpeedUpgrades)
+					progress_bar.size.x+=randf_range(0.25,Manager.printerSpeedUpgrades)
 			
 			if progress_bar.size.x>maxBarSize:
 				progress_bar.size.x=maxBarSize
+				await get_tree().create_timer(randf_range(0.1,0.5)).timeout
+				if !onMainScreen:
+					onMainScreen=true
 			
+	elif compOn:
+		home_screen.visible=true
 
 
 func _on_interaction_component_interacted() -> void:
@@ -51,4 +58,5 @@ func _on_interaction_component_interacted() -> void:
 		player.blurred=false
 		compOn=false
 		await get_tree().create_timer(0.5).timeout
-		
+		onMainScreen=false
+		home_screen.visible=false
